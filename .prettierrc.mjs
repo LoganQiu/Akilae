ccd218a0501cf7c255368200fce6abad4c0317e7
0x7C00 <loganqiu1020@outlook.com>
2025-09-03 15:17:36 +0800

    change h1 style && add prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,22 @@
+/** @type {import("prettier").Config} */
+export default {
+  arrowParens: "avoid",
+  semi: true,
+  tabWidth: 2,
+  printWidth: 80,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  trailingComma: "es6",
+  bracketSpacing: true,
+  endOfLine: "lf",
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+  tailwindStylesheet: "./src/styles/tailwindcss.css",
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
